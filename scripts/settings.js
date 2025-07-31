9028a896c1eef2843389fc7e0c72af501a5d07a7
caydenbu <cayden.q.buchanan@gmail.com>
2025-07-30 18:51:31 -0700

    added settings button to main screen

--- scripts/settings.js
+++ scripts/settings.js
@@ -13,12 +13,16 @@ function ToggleSettings() {
 // Slider logic
 const MUSICSLIDER = document.getElementById("music-slider");
 MUSICSLIDER.addEventListener("input", () => {
-    musicVol = MUSICSLIDER.value / 100; // keep volume between 0-1
+    musicVol = MUSICSLIDER.value / 100; // keeps volume between 0-1
     jazzBackground.volume = musicVol;
 });
 
 const SFXSLIDER = document.getElementById("sfx-slider");
 SFXSLIDER.addEventListener("input", () => {
-    sfxVol = SFXSLIDER.value / 100; // Line#16
+    sfxVol = SFXSLIDER.value / 100; // keeps volume between 0-1
     shuffle.volume = sfxVol;
 });
+
+// sets volume to previously assigned user volume (defaults to .5 if never changed I think)
+jazzBackground.volume = MUSICSLIDER.value / 100;
+shuffle.volume = SFXSLIDER.value / 100;
